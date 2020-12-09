1f87b007d7a23d92013d2103e28c42d6c9dc14d5
Dzaky <68675197+walkinglele@users.noreply.github.com>
2020-12-09 10:27:47 +0700

    Add files via upload

---
+++ Hijaiyah/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Hijaiyah
+//
+//  Created by Edi Sunardi on 19/11/20.
+//
+ 
+import UIKit
+import AVFoundation
+ 
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+ 
+    
+    @IBAction func btnHijaiyah(_ sender: UIButton) {
+        playSound(sound: sender.currentTitle!)
+    }
+    
+    func playSound(sound: String) {
+ 
+        
+        let url = Bundle.main.url(forResource: sound, withExtension: "mp3")
+        
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+    
+    
+ 
+}
+ 
+
+
